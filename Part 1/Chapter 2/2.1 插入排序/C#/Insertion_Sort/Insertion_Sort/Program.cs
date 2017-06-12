5c57eec7669cb8d77332d72c29894403b1ac387f
xsana <nico@niconi.cn>
2017-06-12 20:40:09 +0800

    修改C#版本为面向对象逻辑
    修改C#版本为面向对象逻辑

--- Part 1/Chapter 2/2.1 插入排序/C#/Insertion_Sort/Insertion_Sort/Program.cs
+++ Part 1/Chapter 2/2.1 插入排序/C#/Insertion_Sort/Insertion_Sort/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,19 +9,21 @@ namespace Insertion_Sort
 {
     class Program
     {
+        int [] a = { 2, 8, 6, 7, 1, 10, 5, 3, 9, 4 };
         static void Main(string[] args)
         {
-            int[] a = { 2, 8, 6, 7, 1, 10, 5, 3, 9, 4 };
+            var temp = new Program();
             Console.WriteLine("初始的数组：");
-            foreach (var num in a)
-            {
-                Console.Write(num + " ");
-            }
-            Console.WriteLine();
-
-            Insertion_Sort(ref a);
+            temp.print();
+            
+            temp.Insertion_Sort();
 
             Console.WriteLine("经过插入排序后的数组：");
+            temp.print();
+        }
+
+        public void print()
+        {
             foreach (var num in a)
             {
                 Console.Write(num + " ");
@@ -28,19 +31,19 @@ namespace Insertion_Sort
             Console.WriteLine();
         }
 
-        static void Insertion_Sort(ref int[] A)
+        public void Insertion_Sort()
         {
-            int length = A.Length;
+            int length = a.Length;
             for (int j = 1; j <= length - 1; ++j)
             {
-                int key = A[j];
+                int key = a[j];
                 int i = j - 1;
-                while (i >= 0 && A[i] > key)
+                while (i >= 0 && a[i] > key)
                 {
-                    A[i + 1] = A[i];
+                    a[i + 1] = a[i];
                     --i;
                 }
-                A[i + 1] = key;
+                a[i + 1] = key;
             }
         }
     }
