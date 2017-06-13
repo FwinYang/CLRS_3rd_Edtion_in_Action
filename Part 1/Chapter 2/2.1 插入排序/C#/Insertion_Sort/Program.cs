6fa0accc5de4c6f91677f4ea8ea409f49a63279d
xsana <nico@niconi.cn>
2017-06-13 14:59:29 +0800

    修改项目结构，与代码逻辑

---
+++ Part 1/Chapter 2/2.1 插入排序/C#/Insertion_Sort/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Insertion_Sort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var classic = new Classic();
+            Console.WriteLine("初始的数组：");
+            classic.print();
+            classic.Insertion_Sort();
+            Console.WriteLine("经过插入排序后的数组：");
+            classic.print();
+            Console.WriteLine("--------------------------------");
+            var modern = new Modern();
+            Console.WriteLine("初始的数组：");
+            classic.print();
+            classic.Insertion_Sort();
+            Console.WriteLine("经过插入排序后的数组：");
+            classic.print();
+        }
+    }
+}
