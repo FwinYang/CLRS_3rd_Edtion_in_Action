e3e2cd1e90b1d257698c5b6b90db4ebbecf23ae5
xsana <nico@niconi.cn>
2017-06-13 15:14:54 +0800

    添加Java版本，修改C#版本小错误

--- Part 1/Chapter 2/2.1 插入排序/C#/Insertion_Sort/Program.cs
+++ Part 1/Chapter 2/2.1 插入排序/C#/Insertion_Sort/Program.cs
@@ -15,10 +15,10 @@ namespace Insertion_Sort
             Console.WriteLine("--------------------------------");
             var modern = new Modern();
             Console.WriteLine("初始的数组：");
-            classic.print();
-            classic.Insertion_Sort();
+            modern.print();
+            modern.Insertion_Sort();
             Console.WriteLine("经过插入排序后的数组：");
-            classic.print();
+            modern.print();
         }
     }
 }
